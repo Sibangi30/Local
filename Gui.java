1548c7c1e45b8f999dfc714ca95120de0de7d722
Sibangi30 <Sibangishree@gmail.com>
2026-03-08 23:42:28 +0530

    learning Gui interface

---
+++ Gui.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import javax.swing.*;
+
+import javax.swing.JFrame;
+
+public class Gui {
+    public static void main(String args[]){
+        JFrame frame=new JFrame("Main Frame");
+        frame.setSize(400,400);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
